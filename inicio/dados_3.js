f96cb48904e63fcf5fd9daeace55fa89bc833770
Karina  Moura <kahmourasouza33@gmail.com>
2021-10-01 15:17:04 -0300

    Segunda parte dos exercícios

---
+++ inicio/dados_3.js
@@ -0,0 +1,11 @@
+let idade = 31; 
+console.log(typeof 31)
+console.log(typeof idade);
+
+let salario = 4578.32; // Float
+console.log(typeof salario);
+
+let estaChovendo = true // or false //boolean
+console.log(typeof estaChovendo);
+console.log(typeof "Teste"); //string
+console.log(typeof 'Tete');
---
+++ inicio/desafio_area.js
@@ -0,0 +1,9 @@
+// PI * raio * raio
+
+let raio = 10;
+const PI = 3.141592
+let areaCirculo = PI * raio * raio;
+
+
+console.log("O valor da área é " + areaCirculo + " " + "m2");
+console.log(Math.PI); // biblioteca Math
